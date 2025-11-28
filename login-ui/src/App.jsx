1d45bb38b81ea4eca1468b2401c3d41252a19181
Ansari Abdul Basit <abdulbasitabdulbaqui@gmail.com>
2025-11-28 19:23:30 +0530

    making new

---
+++ login-ui/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import NavBarCom from "./components/NavBarCom"
+import About from "./components/About"
+import Contact from "./components/Contact"
+const App = () => {
+  return (
+    <div>
+      <NavBarCom />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ login-ui/src/components/NavBarCom.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+export const NavBarCom = () => {
+  const [isLogin, setIsLogIn] = useState(false);
+  const handleLogin = () => setIsLogIn("true");
+  const handleLogout = () => setIsLogIn("false");
+
+  return (
+    <div>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container style={{ width: "100%" }}>
+          <Navbar.Brand href="#home">E-Commerce</Navbar.Brand>
+          <Navbar.Toggle aria-controls="main-navbar" />
+          <Navbar.Collapse
+            id="main-navbar"
+            className="d-flex justify-content-between"
+          >
+            <Nav className="me-auto">
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+            {!isLogin && (
+              <Button onClick={handleLogin} style={{ marginRight: "20px" }}>
+                Login
+              </Button>
+            )}
+            {isLogin && (
+              <Button onClick={handleLogout} style={{ marginRight: "20px" }}>
+                Logout
+              </Button>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+export default NavBarCom;
