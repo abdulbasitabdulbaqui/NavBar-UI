22337dd48aa02c4b68233c8c6b1c685451e07465
Ansari Abdul Basit <abdulbasitabdulbaqui@gmail.com>
2025-11-29 13:15:11 +0530

    solve the login logout issue

--- login-ui/src/components/NavBarCom.jsx
+++ login-ui/src/components/NavBarCom.jsx
@@ -5,8 +5,8 @@ import { Button } from "react-bootstrap";
 
 export const NavBarCom = () => {
   const [isLogin, setIsLogIn] = useState(false);
-  const handleLogin = () => setIsLogIn("true");
-  const handleLogout = () => setIsLogIn("false");
+  const handleLogin = () => setIsLogIn(true);
+  const handleLogout = () => setIsLogIn(false);
 
   return (
     <div>
